01b29182ad83404c2029f47270eb76452d1cf5b7
Anton <94398153+ToniNov@users.noreply.github.com>
2023-01-09 00:00:04 +0300

    Delete dist directory

--- dist/models/Tag.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const TagSchema = new mongoose_1.Schema({
-    _id: mongoose_1.Types.ObjectId,
-    title: { type: String, required: true },
-}, { collection: 'tags', timestamps: true });
-const TagModel = (0, mongoose_1.model)('Tag', TagSchema);
-exports.default = TagModel;
--- dist/models/Topic.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const TopicSchema = new mongoose_1.Schema({
-    _id: mongoose_1.Types.ObjectId,
-    title: { type: String, required: true },
-}, { collection: 'topics', timestamps: true });
-const TopicModel = (0, mongoose_1.model)('Topic', TopicSchema);
-exports.default = TopicModel;
